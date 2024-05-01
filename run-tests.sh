ae21cbf047bdd2f6c46430050e3671fc92e55add
Peter Weber <peter.weber@rero.ch>
2024-05-01 11:08:54 +0200

    project: update dependencies
    * Bumps dependencies.
    * Adds `docker-services` parameter to `run-tests``
        to allow starting of necessary services for test.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
 # INVENIO-SIP2
@@ -16,37 +16,83 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-RED='\033[0;31m'
-GREEN='\033[0;0;32m'
-NC='\033[0m' # No Color
+# COLORS for messages
+NC='\033[0m'                    # Default color
+INFO_COLOR='\033[1;97;44m'      # Bold + white + blue background
+SUCCESS_COLOR='\033[1;97;42m'   # Bold + white + green background
+ERROR_COLOR='\033[1;97;41m'     # Bold + white + red background
 
 SCRIPT_PATH=$(dirname "$0")
 
-display_error_message () {
-	echo -e "${RED}$1${NC}" 1>&2
+PROGRAM=`basename $0`
+SCRIPT_PATH=$(dirname "$0")
+
+# MESSAGES
+msg() {
+  echo -e "${1}" 1>&2
+}
+# Display a colored message
+# More info: https://misc.flogisoft.com/bash/tip_colors_and_formatting
+# $1: choosen color
+# $2: title
+# $3: the message
+colored_msg() {
+  msg "${1}[${2}]: ${3}${NC}"
 }
 
-display_success_message () {
-    echo -e "${GREEN}$1${NC}" 1>&2
+info_msg() {
+  colored_msg "${INFO_COLOR}" "INFO" "${1}"
 }
 
-display_error_message_and_exit () {
-  display_error_message "$1"
-  exit 1
+error_msg() {
+  colored_msg "${ERROR_COLOR}" "ERROR" "${1}"
+}
+
+error_msg+exit() {
+  error_msg "${1}" && exit 1
+}
+
+success_msg() {
+  colored_msg "${SUCCESS_COLOR}" "SUCCESS" "${1}"
+}
+
+success_msg+exit() {
+  colored_msg "${SUCCESS_COLOR}" "SUCCESS" "${1}" && exit 0
+}
+
+# Displays program name
+msg "PROGRAM: ${PROGRAM}"
+
+function tests () {
+  set -e
+  info_msg "Test pydocstyle:"
+  pydocstyle invenio_sip2 tests docs
+
+  info_msg "Test isort:"
+  isort invenio_sip2 tests --check-only --diff
+
+  info_msg "Test useless imports:"
+  autoflake --recursive --remove-all-unused-imports --ignore-init-module-imports --check-diff --quiet .
+
+  info_msg "Sphinx-build:"
+  sphinx-build -qnNW docs docs/_build/html
+
+  info_msg "Tests:"
+  poetry run pytest
 }
 
 if [ $# -eq 0 ]
-    then
-        set -e
-        display_success_message "Test pydocstyle:"
-        pydocstyle invenio_sip2 tests docs
-        display_success_message "Test isort:"
-        isort invenio_sip2 tests --check-only --diff
-        echo -e ${GREEN}Test useless imports:${NC}
-        autoflake --remove-all-unused-imports -c -r --ignore-init-module-imports . || display_error_message_and_exit "\nUse this command to check imports:\n\tautoflake --remove-all-unused-imports -r --ignore-init-module-imports .\n"
-        display_success_message "Sphinx-build:"
-        sphinx-build -qnNW docs docs/_build/html
-        display_success_message "Tests:"
-#        python setup.py test
-        poetry run pytest
+  then
+    tests
+    tests_exit_code=$?
+    exit "$tests_exit_code"
+fi
+
+if [ "$1" = "docker-services" ]
+  then
+    eval "$(docker-services-cli up --db ${DB:-postgresql} --cache ${CACHE:-redis} --mq ${MQ:-redis} --env)"
+    tests
+    tests_exit_code=$?
+    docker-services-cli down
+    exit "$tests_exit_code"
 fi
